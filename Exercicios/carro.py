9cccc771b0177c19ab5b8cf0b5eb59b70540a397
Jackez01 <lucasgefreitas@gmail.com>
2026-04-07 15:39:55 -0300

    Exercicios de classe.

---
+++ Exercicios/carro.py
@@ -0,0 +1,3 @@
+# Implemente uma classe chamada Carro com os atributos básicos, 
+# como modelo, cor e ano. 
+# Crie uma instância dessa classe e atribua valores aos seus atributos.
--- Exercicios/musica.py
+++ Exercicios/musica.py
@@ -4,14 +4,27 @@
 
 # Por exemplo, uma classe Música poderia ter 3 atributos (que trazem as características ou propriedades de um objeto):
 
+# class Musica:
+#     nome = ''
+#     artista = ''
+#     duracao = int
+
+# musica1 = Musica()
+# musica1.nome = 'Nothing else metters'
+# musica1.artista = 'Metallica'
+# musica1.duracao = 3
+
+# print(vars(musica1))
+
+#Pode fazer dessa forma também
 class Musica:
-    nome = ''
-    artista = ''
-    duracao = int
+    def __init__(self, nome ='', artista ='', duracao=0):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
 
-musica1 = Musica()
-musica1.nome = 'Nothing else metters'
-musica1.artista = 'Metallica'
-musica1.duracao = 3
+musica1 = Musica(nome='the day never coming', artista='Metallica', duracao=5)
+musica2 = Musica(nome='Blue', artista = 'Billie Elish', duracao = 3)
+musica3 = Musica(nome = 'A sua maneira', artista = 'Capital inicial', duracao = 4)
 
-print(vars(musica1))
+print (vars(musica1))
