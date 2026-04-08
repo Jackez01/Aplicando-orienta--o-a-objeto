a3f7b6676489bd865d680d717c9005b05a96be44
Lucas Geraldi Freitas <lucasgefreitas@gmail.com>
2026-04-07 21:56:15 -0300

    carro

--- Exercicios/carro.py
+++ Exercicios/carro.py
@@ -1,3 +1,6 @@
 # Implemente uma classe chamada Carro com os atributos básicos, 
 # como modelo, cor e ano. 
 # Crie uma instância dessa classe e atribua valores aos seus atributos.
+
+class Carro:
+    def __init__(self,modelo ='', ):
