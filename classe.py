ae0a8dd7714fa20b548848bb1ebcc8470cd07e92
Lucas Geraldi Freitas <lucasgefreitas@gmail.com>
2026-04-07 23:08:24 -0300

    Atualização da classe Restaurante.

--- classe.py
+++ classe.py
@@ -4,7 +4,8 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        #isso faz com que o ativo fique privado
+        self._ativo = False
         Restaurante.restaurantes.append(self)
         
         #serve para mostrar o testo em formato de string
@@ -12,8 +13,13 @@ class Restaurante:
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
+        print (f'{'Nome do restaurante:'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'} ')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+    #modifica como o atributo vai ser lido
+    @property
+    def ativo(self):
+        return 'verdadeiro' if self._ativo else 'false'
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
