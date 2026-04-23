166a88896ebe25a88bbb8c3832dfd3f78ee8f683
Jackez01 <lucasgefreitas@gmail.com>
2026-04-23 14:14:05 -0300

    Exercicios de conjuntos e dicionarios.

---
+++ Exercicios conjuntos e dicionarios/palavra_comum.py
@@ -0,0 +1,1 @@
+# Clara é editora de uma revista e deseja comparar dois artigos para identificar quais palavras aparecem em ambos. Sua tarefa é criar um programa que receba dois textos e exiba o conjunto de palavras comuns entre eles.
